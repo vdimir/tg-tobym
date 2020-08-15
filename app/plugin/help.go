9c2897f2502c79c04362ff6e727399be3c8bbd16
vdimir <vdimirc@gmail.com>
2020-08-15 17:00:39 +0300

    Add bot name for help in chats

--- app/plugin/help.go
+++ app/plugin/help.go
@@ -17,10 +17,10 @@ type Help struct {
 	Cmds []CommandDescription
 }
 
-func (plg *Help) formatHelpText(short bool) string {
+func (plg *Help) formatHelpText(short bool, botName string) string {
 	lines := []string{}
 	for _, cmd := range plg.Cmds {
-		lines = append(lines, fmt.Sprintf("/%s - %s", cmd.Cmd, cmd.Help))
+		lines = append(lines, fmt.Sprintf("/%s%s - %s", cmd.Cmd, botName, cmd.Help))
 		if !short && cmd.Details != "" {
 			lines = append(lines, cmd.Details)
 		}
@@ -44,8 +44,16 @@ func (plg *Help) HandleUpdate(ctx context.Context, upd *tgbotapi.Update) (bool, 
 
 	msgToMe := plg.Bot.IsMessageToMe(*upd.Message) || upd.Message.Chat.IsPrivate()
 	if cmd := upd.Message.Command(); msgToMe && cmd == "help" || cmd == "help_short" {
+		botName := ""
+		if !upd.Message.Chat.IsPrivate() {
+			botMe, err := plg.Bot.GetMe()
+			if err != nil {
+				return true, err
+			}
+			botName = "@" + botMe.UserName
+		}
 		args := upd.Message.CommandArguments()
-		text := plg.formatHelpText(args == "short" || cmd == "help_short")
+		text := plg.formatHelpText(args == "short" || cmd == "help_short", botName)
 		err := common.ReplyWithText(plg.Bot, upd.Message, text, "")
 		return true, err
 	}
